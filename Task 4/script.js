378fe7a4c7400aa1f1a4c852a31b6ddd45cad961
Aryan Saini <155607558+aryansainias@users.noreply.github.com>
2024-11-23 10:13:03 +0530

    Add files via upload

---
+++ Task 4/script.js
@@ -0,0 +1,8 @@
+const buttoncolarchange = document.getElementById("Buttonchangecolur");
+
+
+buttoncolarchange.addEventListener("click", function() {
+   
+    document.body.style.backgroundColor = "#ffeb3b";
+});
+
